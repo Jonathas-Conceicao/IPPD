b9272af5a7f876c2adcb0d60a7f53af7abffdb80
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2018-04-22 21:23:14 -0300

    Theses nonblocking primitives are weird

--- Key-Find/key_find.c
+++ Key-Find/key_find.c
@@ -39,7 +39,6 @@ void insertNewKeycList(cList_t **head, int key) {
 	*head = node;
 }
 
-
 cList_t *initcList(int array[], int size) {
 	assert(size >= 1);
 	cList_t *head;
@@ -50,13 +49,13 @@ cList_t *initcList(int array[], int size) {
 	return head;
 }
 
-void printAllcList(cList_t *head) {
-	cList_t *curr = head;
-	do {
-		printf("Key: %i\n", curr->key);
-		curr = curr->prev;
-	} while (curr != head);
-}
+/* void printAllcList(cList_t *head) { */
+/* 	cList_t *curr = head; */
+/* 	do { */
+/* 		printf("Key: %i\n", curr->key); */
+/* 		curr = curr->prev; */
+/* 	} while (curr != head); */
+/* } */
 
 int main (void) {
 	int keys[SIZE];
@@ -64,35 +63,31 @@ int main (void) {
 		keys[i] = i;
 	}
 	char done = 0;
-  int target = 313;
+	int target = 313;
 	int my_rank;
 	int comm_sz;
-	MPI_Request req;   // Required variable for non-blocking calls
-	MPI_Status stat;
+	MPI_Request req;
 
-	cList_t *head = initcList(keys, SIZE);
-	cList_t *curr = head;
+	cList_t *curr = initcList(keys, SIZE);
 
 	MPI_Init(NULL, NULL);
 	MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-	int other_process = (my_rank + 1) % comm_sz;
-
 	assert(comm_sz == 2);
+
+	int other_process = (my_rank + 1) % comm_sz;
+	MPI_Irecv(&done, 1, MPI_CHAR, other_process, 0, MPI_COMM_WORLD, &req);
 	do {
 		if (curr->key == target) { // If the key was found
 			done = 1;
 			printf("I, process %i, found the key, sedding signal to %i so it can stop working!\n", my_rank, other_process);
 			MPI_Send(&done, 1, MPI_CHAR, other_process, 0, MPI_COMM_WORLD);
 		} else {
-			curr = (my_rank) ? curr->next : curr->prev; // Process search in different directions
-  		MPI_Irecv(&done, 1, MPI_CHAR, other_process, 0, MPI_COMM_WORLD, &req);
-  	}
-		
+			curr = (my_rank) ? curr->prev : curr->next; // Process search in different directions
+			MPI_Test(&req, (int *) &done, MPI_STATUS_IGNORE);
+		}
 	} while(!done);
-	MPI_Wait(&req, &stat);
 
 	MPI_Finalize();
-
 	return 0;
 }
