6ed1b008159a7d504dfad34c9750d09db44ab6cb
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2018-04-25 15:03:22 -0300

    not quite working

--- Merge-Sort/merge_sort_mpi.c
+++ Merge-Sort/merge_sort_mpi.c
@@ -4,7 +4,7 @@
 #include <mpi.h>
 
 #define SEED 313
-#define SIZE 20
+#define SIZE 8
 
 typedef struct merge_mpi_data_ {
 	int my_rank;
@@ -24,13 +24,15 @@ void merge_sort(merge_mpi_data *data);
 void merge_generate_random_array(merge_mpi_data *data, int seed, int size);
 merge_mpi_data *merge_init();
 void merge_finallize();
+void swap(int *a, int *b);
 
 int main (void) {
 	merge_mpi_data *data;
 	data = merge_init();
-  merge_generate_random_array(data, SEED, SIZE);
+	merge_generate_random_array(data, SEED, SIZE);
 	merge_print_array(data);
 	merge_sort(data);
+	merge_print_array(data);
 	merge_finallize();
 	return 0;
 }
@@ -57,7 +59,8 @@ void merge_generate_random_array(merge_mpi_data *data, int seed, int size) {
 	assert(data->array);
 	data->size = size;
 	for (int i = 0; i < size; ++i) {
-		data->array[i] = rand() % 1000; // TODO: Eu tenho que remover isso aqui antes de entregar
+		data->array[i] = rand() % 1000;
+		/* data->array[i] = i; */
 	}
 	return;
 }
@@ -79,32 +82,64 @@ void merge_sort(merge_mpi_data *data) {
 		MPI_Recv(&data->l_range, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 		MPI_Recv(&data->h_range, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 	}
-	printf("For %i, l_range: %i, h_range: %i\n", data->my_rank, data->l_range, data->h_range);
+	/* printf("For %i, l_range: %i, h_range: %i\n", data->my_rank, data->l_range, data->h_range); */
 	merge_split(data->array, data->l_range, data->h_range);
+	if (data->my_rank == 0) {
+		merge_stack(data->array, data->l_range, data->h_range);
+	}
 }
 
 void merge_split(int *array, int l_range, int h_range) {
-	printf("Spliting:\n");
+	if (l_range >= h_range -1)
+		return;
 	int split_pos = (l_range+h_range)/2;
-	char *h1 = array_to_string(array+l_range, split_pos - l_range);
-	char *h2 = array_to_string(array+split_pos, h_range - split_pos);
-	printf("h1: %s\nh2: %s\n", h1, h2);
-	free(h1);
-	free(h2);
+	/* char *ar = array_to_string(array+l_range, h_range-l_range); */
+	/* char *h1 = array_to_string(array+l_range, split_pos - l_range); */
+	/* char *h2 = array_to_string(array+split_pos, h_range - split_pos); */
+	/* printf("Spliting: %s\n", ar); */
+	/* printf("h1:       %s\nh1:       %s\n\n", h1, h2); */
+	/* free(ar); */
+	/* free(h1); */
+	/* free(h2); */
+	merge_split(array, l_range, split_pos);
+	merge_split(array, split_pos, h_range);
+	merge_stack(array, l_range, h_range);
+	return;
 }
 
-void merge_stack() {
-	
+void merge_stack(int *array, int l_range, int h_range) {
+	int split_pos = (l_range+h_range)/2;
+	int *left = array+l_range;
+	int *right = array+split_pos;
+	/* printf("Stacking!\n"); */
+	int l_p = 0;
+	int r_p = 0;
+	while (l_p < split_pos && r_p < h_range) {
+		if (left[l_p] > right[r_p]) {
+			swap(&left[l_p], &right[r_p]);
+			++r_p;
+			++l_p;
+		}
+		++l_p;
+	}
+	return;
+}
+
+void swap(int *a, int *b) {
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+	return;
 }
 
 char *array_to_string(int *array, int size) {
-	char *s = malloc( sizeof(char) * (3+(6*size)) );
+	char *s = malloc( sizeof(int) * size );
 	assert(s);
 	sprintf(s, "[");
 	for (int i = 0; i < size-1; ++i) {
 		sprintf(s, "%s%i, ", s, array[i]);
 	}
-	sprintf(s, "%s%i]", s, array[size-1]);
+	sprintf(s, "%s%i]%c", s, array[size-1], '\0');
 	return s;
 }
 
@@ -112,9 +147,9 @@ void merge_print_array(merge_mpi_data *data) {
 	if (data->my_rank != 0)
 		return;
 	printf("Full array:\n");
-  char *s = array_to_string(data->array, data->size);
+	char *s = array_to_string(data->array, data->size);
 	printf("%s\n", s);
-	free(s);
+	/* free(s); */
 	return;
 }
 
