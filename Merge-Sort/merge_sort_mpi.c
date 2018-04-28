395bf49f406fef7c6aab04702bc9675594bf0132
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2018-04-28 20:33:35 -0300

    Merge reworked and working, code is still messy and probably still need some proper deallocation

--- Merge-Sort/merge_sort_mpi.c
+++ Merge-Sort/merge_sort_mpi.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-#include <mpi.h>
+/* #include <mpi.h> */
 
 #define SEED 313
 #define SIZE 8
@@ -11,15 +11,14 @@ typedef struct merge_mpi_data_ {
 	int comm_sz;
 	int *array;
 	int size;
-	int h_range;
-	int l_range;
+	int *sorted_array;
 } merge_mpi_data;
 
 int is_in_list(int key, int *l, int s);
 void merge_print_array(merge_mpi_data *data);
 char *array_to_string(int *array, int size);
-void merge_stack();
-void merge_split(int *array, int split_pos, int size);
+int  *merge_stack(int *left, int left_size, int *right, int right_size);
+int  *merge_split(int *array, int split_pos, int size);
 void merge_sort(merge_mpi_data *data);
 void merge_generate_random_array(merge_mpi_data *data, int size, int seed);
 merge_mpi_data *merge_init();
@@ -38,18 +37,20 @@ int main (void) {
 }
 
 merge_mpi_data *merge_init() {
-	MPI_Init(NULL, NULL);
+	/* MPI_Init(NULL, NULL); */
 	merge_mpi_data *ret = malloc(sizeof(merge_mpi_data));
 	assert(ret);
-	MPI_Comm_size(MPI_COMM_WORLD, &ret->comm_sz);
-	MPI_Comm_rank(MPI_COMM_WORLD, &ret->my_rank);
+	/* MPI_Comm_size(MPI_COMM_WORLD, &ret->comm_sz); */
+	ret->comm_sz = 1;
+	/* MPI_Comm_rank(MPI_COMM_WORLD, &ret->my_rank); */
+	ret->my_rank = 0;
 	ret->array = NULL;
 	ret->size = 0;
 	return ret;
 }
 
 void merge_finallize() {
-	MPI_Finalize();
+	/* MPI_Finalize(); */
 	return;
 }
 
@@ -66,32 +67,14 @@ void merge_generate_random_array(merge_mpi_data *data, int size, int seed) {
 }
 
 void merge_sort(merge_mpi_data *data) {
-	if (data->my_rank == 0) {
-		int l_range;
-		int h_range;
-		int step = data->size / data->comm_sz;
-		for (int i = 1; i < data->comm_sz; ++i) { // Skips 0 so it doesn't send a message to itself
-			l_range = step*i;
-			h_range = step*(i+1);
-			MPI_Send(&l_range, 1, MPI_INT, i, 0, MPI_COMM_WORLD); // Sends lower bound
-			MPI_Send(&h_range, 1, MPI_INT, i, 1, MPI_COMM_WORLD); // Sends upper bound
-		}
-		data->l_range = 0;
-		data->h_range = step;
-	} else {
-		MPI_Recv(&data->l_range, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		MPI_Recv(&data->h_range, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-	}
-	/* printf("For %i, l_range: %i, h_range: %i\n", data->my_rank, data->l_range, data->h_range); */
-	merge_split(data->array, data->l_range, data->h_range);
-	if (data->my_rank == 0) {
-		merge_stack(data->array, data->l_range, data->h_range);
-	}
+	data->sorted_array = merge_split(data->array, 0, data->size);
+	free(data->array);
+	data->array = data->sorted_array;
 }
 
-void merge_split(int *array, int l_range, int h_range) {
-	if (l_range >= h_range -1)
-		return;
+int *merge_split(int *array, int l_range, int h_range) {
+	if (l_range >= h_range -1) // Stop recursion
+		return (array+l_range);
 	int split_pos = (l_range+h_range)/2;
 	/* char *ar = array_to_string(array+l_range, h_range-l_range); */
 	/* char *h1 = array_to_string(array+l_range, split_pos - l_range); */
@@ -101,28 +84,36 @@ void merge_split(int *array, int l_range, int h_range) {
 	/* free(ar); */
 	/* free(h1); */
 	/* free(h2); */
-	merge_split(array, l_range, split_pos);
-	merge_split(array, split_pos, h_range);
-	merge_stack(array, l_range, h_range);
-	return;
+	int *left   = merge_split(array, l_range, split_pos);
+	int *right  = merge_split(array, split_pos, h_range);
+	int *sorted = merge_stack(left, (split_pos - l_range), right, (h_range - split_pos) );
+	return sorted;
 }
 
-void merge_stack(int *array, int l_range, int h_range) {
-	int split_pos = (l_range+h_range)/2;
-	int *left = array+l_range;
-	int *right = array+split_pos;
-	/* printf("Stacking!\n"); */
-	int l_p = 0;
-	int r_p = 0;
-	while (l_p < split_pos && r_p < h_range) {
-		if (left[l_p] > right[r_p]) {
-			swap(&left[l_p], &right[r_p]);
+int *merge_stack(int *left, int left_size, int *right, int right_size) {
+	int new_array_size = (left_size + right_size);
+	int *new_array = malloc(sizeof(int) * new_array_size);
+	assert(new_array);
+	for (int n_p = 0, r_p = 0, l_p = 0; n_p < new_array_size; ++n_p) {
+		if (l_p < left_size) { 
+			if (r_p < right_size) {
+				if (left[l_p] < right[r_p]) {
+					new_array[n_p] = left[l_p];
+					++l_p;
+				} else {
+					new_array[n_p] = right[r_p];
+					++r_p;
+				}
+			} else {
+				new_array[n_p] = left[l_p];
+				++l_p;
+			}
+		} else {
+			new_array[n_p] = right[r_p];
 			++r_p;
-			++l_p;
 		}
-		++l_p;
 	}
-	return;
+	return new_array;
 }
 
 void swap(int *a, int *b) {
