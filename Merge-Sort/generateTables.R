be29843f5e1f1fde8ab5a68b5f656c01f40681d5
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2018-05-03 00:08:41 -0300

    Finally got a proper division

--- Merge-Sort/generateTables.R
+++ Merge-Sort/generateTables.R
@@ -3,10 +3,11 @@ n     <- as.integer(commandArgs(trailingOnly=TRUE)[1])
 times <- as.integer(commandArgs(trailingOnly=TRUE)[2])
 file  <- commandArgs(trailingOnly=TRUE)[3]
 for (i in 0:(times - 1)) {
-	table <- read.csv(file, header=FALSE, nrows=n+1, skip=(i*(n+1)))
-	names(table) <- c("Process ID", "Process Time", "Copy Time")
-	print(head(table, -1))
-	total <- tail(table, 1)[1,1]
-	names(total) <- c("Total Time")
-	print(total)
+  table <- read.csv(file, header=FALSE, nrows=n+1, skip=(i*(n+1)))
+  process <- table[complete.cases(table),]
+  total <- table[!complete.cases(table),1]
+  names(process) <- c("Process ID", "Process Time", "Copy Time")
+  names(total) <- c("Total Time")
+  print(process)
+  print(total)
 }
