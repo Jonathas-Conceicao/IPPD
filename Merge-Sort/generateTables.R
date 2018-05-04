eb9341a843864d680725fd60fc3985bf80936a96
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2018-05-04 17:44:48 -0300

    Somework done, but I still hate R

--- Merge-Sort/generateTables.R
+++ Merge-Sort/generateTables.R
@@ -1,5 +1,9 @@
 #!/usr/bin/Rscript
 library(ggplot2)
+library(lattice)
+library(plyr)
+library(Rmisc)
+library(scales)
 
 n     <- as.integer(commandArgs(trailingOnly=TRUE)[1])
 times <- as.integer(commandArgs(trailingOnly=TRUE)[2])
@@ -18,16 +22,35 @@ for (j in 0:(times - 1)) {
   ## names(total)   <- c("Total Time")
   ## print(process)
   for (i in 1:n) {
-    pT[i, j+1] <- process[i, 2] ## Process time line
-    cT[i, j+1] <- process[i, 3] ## Copy time line
-    ## print(sprintf("%f %f", process[i,2], process[i,3]))
+    pT[(j+1)+((i-1) * times), 1] <- process[i, 2] ## Process time line
+    pT[(j+1)+((i-1) * times), 2] <- process[i, 3] ## Copy time line
+    pT[(j+1)+((i-1) * times), 3] <- i
+    ## cT[(j+1)+((i-1) * times), 3] <- i
   }
-  tT[1, j+1] <- total[1]
+  tT[j+1, 1] <- total[1]
 }
 
-print (pT)
-print (cT)
-print (tT)
+## names(pT) <- c("Process Time","Process")
+## names(cT) <- c("Copy Time","Process")
+names(pT) <- c("Process Time", "Copy Time", "Process")
+
+
+## print (pT)
+## print (cT)
+## print (tT)
+
 
 pdf ("test.pdf")
-ggplot( pT, aes( y=(seq(0, 0.0002)) , x= seq(1,4) ) )  + geom_boxplot()
+ggplot( pT, aes(x = Process, y = `Process Time`, group=Process, fill=Process)) +
+  scale_x_discrete(name = "Numero de Processos MPI") +
+  scale_y_continuous(name = "Tempo (s)") +
+  labs(fill="Processos") +
+  ggtitle("Tempo de Processamento") +
+  geom_boxplot()
+
+ggplot( pT, aes(x = Process, y = `Copy Time`, group=Process, fill=Process)) +
+  scale_x_discrete(name = "Numero de Processos MPI") +
+  scale_y_continuous(name = "Tempo (s)") +
+  labs(fill="Processos") +
+  ggtitle("Tempo de Processamento") +
+  geom_boxplot()
