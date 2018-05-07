b84884c9266f69bc38edeabcd8a2be0d284644ab
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2018-05-07 12:59:00 -0300

    R is starting to grow on me

--- Merge-Sort/generateTables.R
+++ Merge-Sort/generateTables.R
@@ -6,7 +6,6 @@ times <- as.integer(commandArgs(trailingOnly=TRUE)[2])
 file  <- commandArgs(trailingOnly=TRUE)[3]
 
 pT <- data.frame()
-cT <- data.frame()
 tT <- data.frame()
 
 table <- read.csv(file, header=FALSE)
@@ -16,26 +15,26 @@ total <- table[!complete.cases(table),c(1,2)]
 names(pT)    <- c("Process", "ID", "Process Time", "Copy Time")
 names(total) <- c("Process", "Total Time")
 
-print (pT)
+print (pT[order(pT$Process,pT$ID),])
 print (total)
 
 pdf ("Medidas.pdf")
-## ggplot( pT, aes(x = Process, y = `Process Time`, group=Process)) +
-##   scale_x_continuous(name = "Numero de Processos MPI") +
-##   scale_y_continuous(name = "Tempo (s)") +
-##   ggtitle("Tempo de Processamento") +
-##   geom_boxplot() +
-##   stat_summary(fun.y=mean, geom="point", shape=20, size=5, color="red", fill="red")
+ggplot( pT, aes(x = Process, y = `Process Time`, group=Process)) +
+  scale_x_continuous(name = "Numero de Processos MPI") +
+  scale_y_continuous(name = "Tempo (ms)") +
+  ggtitle("Tempo de Processamento") +
+  geom_boxplot() +
+  stat_summary(fun.y=mean, geom="point", shape=20, size=5, color="red", fill="red")
 
-## ggplot( pT, aes(x = Process, y = `Copy Time`, group=Process)) +
-##   scale_x_continuous(name = "Numero de Processos MPI") +
-##   scale_y_continuous(name = "Tempo (s)") +
-##   ggtitle("Tempo de Cópia") +
-##   geom_boxplot() +
-##   stat_summary(fun.y=mean, geom="point", shape=20, size=5, color="red", fill="red")
+ggplot( pT, aes(x = Process, y = `Copy Time`, group=Process)) +
+  scale_x_continuous(name = "Numero de Processos MPI") +
+  scale_y_continuous(name = "Tempo (ms)") +
+  ggtitle("Tempo de Cópia") +
+  geom_boxplot() +
+  stat_summary(fun.y=mean, geom="point", shape=20, size=5, color="red", fill="red")
 
 ggplot( total, aes(x = Process, y = `Total Time`, group=Process)) +
-  scale_y_continuous(name = "Tempo (s)") +
+  scale_y_continuous(name = "Tempo (ms)") +
   ggtitle("Tempo total") +
   geom_boxplot() +
   stat_summary(fun.y=mean, geom="point", shape=20, size=5, color="red", fill="red")
