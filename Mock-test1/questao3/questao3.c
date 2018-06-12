8d48fdd139da9698c9f7c6755c791bb71803ac44
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2018-06-12 17:02:35 -0300

    questao3.c done!

--- Mock-test1/questao3/questao3.c
+++ Mock-test1/questao3/questao3.c
@@ -9,67 +9,29 @@ int prime_number (int n, int id, int p);
 void timestamp ();
 
 int main (int argc, char *argv[]) {
-	int i;
 	int id;
-	int ierr;
 	int n;
-	int n_factor;
 	int n_hi;
-	int n_lo;
 	int p;
-	int primes;
-	int primes_part;
-	double wtime;
 
-	n_lo = 1;
 	if (argc >= 2) {
 		n_hi = atoi(argv[1]);
 	} else {
 		n_hi = 262144;
 	}
-	n_factor = 2;
-	ierr = MPI_Init ( &argc, &argv );
-	ierr = MPI_Comm_size ( MPI_COMM_WORLD, &p );
-	ierr = MPI_Comm_rank ( MPI_COMM_WORLD, &id );
+	MPI_Init ( &argc, &argv );
+	MPI_Comm_size ( MPI_COMM_WORLD, &p );
+	MPI_Comm_rank ( MPI_COMM_WORLD, &id );
 
 	if ( id == 0 ) {
 		timestamp ( );
-	/* 	printf ( "\n" ); */
-	/* 	printf ( "PRIME_MPI\n" ); */
-	/* 	printf ( "  C/MPI version\n" ); */
-	/* 	printf ( "\n" ); */
-	/* 	printf ( "  An MPI example program to count the number of primes.\n" ); */
-	/* 	printf ( "  The number of processes is %d\n", p ); */
-	/* 	printf ( "\n" ); */
-	/* 	printf ( "         N        Pi          Time\n" ); */
-	/* 	printf ( "\n" ); */
 	}
 
-	n = n_lo;
+	n = n_hi;
 
-	while ( n <= n_hi ) {
-		if ( id == 0 ) {
-			wtime = MPI_Wtime ( );
-		}
-		ierr = MPI_Bcast ( &n, 1, MPI_INT, 0, MPI_COMM_WORLD );
-
-		primes_part = prime_number ( n, id, p );
-
-		ierr = MPI_Reduce ( &primes_part, &primes, 1, MPI_INT, MPI_SUM, 0, 
-			MPI_COMM_WORLD );
-
-		if ( id == 0 ) {
-			wtime = MPI_Wtime ( ) - wtime;
-			/* printf ( "  %8d    %8d  %14f\n", n, primes, wtime ); */
-		}
-		n = n * n_factor;
-	}
-	ierr = MPI_Finalize ( );
+  prime_number ( n, id, p );
+	MPI_Finalize ( );
 	if ( id == 0 ) {
-		/* printf ( "\n"); */
-		/* printf ( "PRIME_MPI - Master process:\n"); */
-		/* printf ( "  Normal end of execution.\n"); */
-		/* printf ( "\n" ); */
 		timestamp ( );
 	}
 
@@ -84,21 +46,21 @@ int prime_number (int n, int id, int p) {
 
 	total = 0;
 
-	for ( i = 2 + id; i <= n; i = i + p ) {
+	printf("[");
+	for ( i = 1 + id; i <= n; i = i + p ) {
 		prime = 1;
 		for ( j = 2; j < i; j++ ) {
 			if ( ( i % j ) == 0 ) {
 				prime = 0;
 				break;
-			} else {
 			}
 		}
 		if (prime) {
-			printf("Prime? %i\n", i);
+			printf("%i, ", i);
 		}
 		total = total + prime;
 	}
-	printf("For %i returning %i\n", n, total);
+	printf("]\n");
 	return total;
 }
 
@@ -107,16 +69,16 @@ void timestamp (void) {
 
 	static char time_buffer[TIME_SIZE];
 	const struct tm *tm;
-	size_t len;
 	time_t now;
 
 	now = time ( NULL );
 	tm = localtime ( &now );
 
-	len = strftime ( time_buffer, TIME_SIZE, "%d %B %Y %I:%M:%S %p", tm );
+  strftime ( time_buffer, TIME_SIZE, "%d %B %Y %I:%M:%S %p", tm );
 
 	printf ( "%s\n", time_buffer );
 
 	return;
 # undef TIME_SIZE
 }
+
