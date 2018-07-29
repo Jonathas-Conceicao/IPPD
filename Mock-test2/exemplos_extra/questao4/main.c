ee70b5dd134fcbbc1e4ee7a9d2ba7bd31d9b16cf
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2018-07-29 01:19:09 -0300

    Parallel generation of Matrix and some tweaks

--- Mock-test2/exemplos_extra/questao4/main.c
+++ Mock-test2/exemplos_extra/questao4/main.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <omp.h>
 
 #define CL_USE_DEPRECATED_OPENCL_1_2_APIS
 #include <CL/cl.h>
-
 #define MAX_SOURCE_SIZE (0x100000)
 
-
-#define MATRIX_SIZE 64
-#define WORK_SIZE 8   // Has to be sqrt of MATRIX_SIZE for matrix_display only
+/* #define MATRIX_SIZE 10000 */
+#define MATRIX_SIZE 16 // Smaller to quick tests
+#define WORK_SIZE 64
 #define RANDOM_SEED 313
 #define MAX_RAND 100
 
-/* void matrix_mult(int **A, int **B, int **C); */
-int* new_random_matrix(int size);
-int* new_empty_matrix(int size);
+int* new_random_matrix(int size, int empty);
 void matrix_mult(int *A, int *B, int *C);
 void matrix_display(int *M);
 
@@ -24,12 +22,13 @@ int main(void) {
 	int *C;
 	srand(RANDOM_SEED);
 
-	A = new_random_matrix(MATRIX_SIZE);
-	B = new_random_matrix(MATRIX_SIZE);
-	C = new_empty_matrix(MATRIX_SIZE);
+	A = new_random_matrix(MATRIX_SIZE, 0);
+	B = new_random_matrix(MATRIX_SIZE, 0);
+	C = new_random_matrix(MATRIX_SIZE, 1);
+	printf("Matrix generated\n");
 
-	matrix_display(A);
-	matrix_display(B);
+	/* matrix_display(A); */
+	/* matrix_display(B); */
 	matrix_mult(A, B, C);
 	matrix_display(C);
 
@@ -39,21 +38,25 @@ int main(void) {
 	return 0;
 }
 
-int* new_random_matrix(int size) {
+int* new_random_matrix(int size, int empty) {
 	int *matrix;
+  size *= size;
 	matrix = malloc(sizeof(int) * size);
 	if (!matrix) {
 		fprintf(stderr, "Failed to allocated memory for Matrix");
 		exit(1);
 	}
+	if (empty) return matrix;
+#pragma omp parallel for schedule(static)
 	for(int i = 0; i < size; ++i) {
-		matrix[i] = rand() % MAX_RAND;
+		matrix[i] = rand() % MAX_RAND; // Rand function is thread safe
 	}
 	return matrix;
 }
 
 int* new_empty_matrix(int size) {
 	int *matrix;
+  size *= size;
 	matrix = malloc(sizeof(int) * size);
 	if (!matrix) {
 		fprintf(stderr, "Failed to allocated memory for Matrix");
@@ -65,10 +68,10 @@ int* new_empty_matrix(int size) {
 void matrix_display(int *M) {
 	int i, j;
 	printf("[");
-	for (i = 0; i < WORK_SIZE; ++i) {
+	for (i = 0; i < MATRIX_SIZE; ++i) {
 		printf("[");
-		for (j = 0; j < WORK_SIZE; ++j) {
-			printf("%i, ", M[(i * WORK_SIZE) + j]);
+		for (j = 0; j < MATRIX_SIZE; ++j) {
+			printf("%i, ", M[(i * MATRIX_SIZE) + j]);
 		}
 		printf("\b\b]\n");
 	}
@@ -83,7 +86,7 @@ void matrix_mult(int *A, int *B, int *C) {
 	fp = fopen("matrix_mult_kernel.cl", "r");
 	if (!fp) {
 		fprintf(stderr, "Failed to load kernel.\n");
-		exit(1);
+		exit(2);
 	}
 	source_str = (char*)malloc(MAX_SOURCE_SIZE);
 	source_size = fread( source_str, 1, MAX_SOURCE_SIZE, fp);
@@ -101,18 +104,18 @@ void matrix_mult(int *A, int *B, int *C) {
 	cl_context context = clCreateContext( NULL, 1, &device_id, NULL, NULL, &ret);
 	cl_command_queue command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
 	cl_mem a_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY, 
-																		MATRIX_SIZE * sizeof(int), NULL, &ret);
+																		MATRIX_SIZE * MATRIX_SIZE * sizeof(int), NULL, &ret);
 	cl_mem b_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY,
-																		MATRIX_SIZE * sizeof(int), NULL, &ret);
+																		MATRIX_SIZE * MATRIX_SIZE * sizeof(int), NULL, &ret);
 	cl_mem c_mem_obj = clCreateBuffer(context, CL_MEM_WRITE_ONLY, 
-																	  MATRIX_SIZE * sizeof(int), NULL, &ret);
+																	  MATRIX_SIZE * MATRIX_SIZE * sizeof(int), NULL, &ret);
 
 
 	// Copy the lists A and B to their respective memory buffers
 	ret = clEnqueueWriteBuffer(command_queue, a_mem_obj, CL_TRUE, 0,
-														 MATRIX_SIZE * sizeof(int), A, 0, NULL, NULL);
+														 MATRIX_SIZE * MATRIX_SIZE * sizeof(int), A, 0, NULL, NULL);
 	ret = clEnqueueWriteBuffer(command_queue, b_mem_obj, CL_TRUE, 0, 
-														 MATRIX_SIZE * sizeof(int), B, 0, NULL, NULL);
+														 MATRIX_SIZE * MATRIX_SIZE * sizeof(int), B, 0, NULL, NULL);
 
 	// Create a program from the kernel source
 	cl_program program = clCreateProgramWithSource(context, 1, 
@@ -130,14 +133,14 @@ void matrix_mult(int *A, int *B, int *C) {
 	ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&c_mem_obj);
 		
 	// Execute the OpenCL kernel on the list
-	size_t global_item_size = MATRIX_SIZE; // Process the entire lists
+	size_t global_item_size = MATRIX_SIZE * MATRIX_SIZE; // Process the entire lists
 	size_t local_item_size = WORK_SIZE; // Divide work items into groups of 64
 	ret = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, 
 															 &global_item_size, &local_item_size, 0, NULL, NULL);
 
 	// Read the memory buffer C on the device to the local variable C
 	ret = clEnqueueReadBuffer(command_queue, c_mem_obj, CL_TRUE, 0, 
-														MATRIX_SIZE * sizeof(int), C, 0, NULL, NULL);
+														MATRIX_SIZE * MATRIX_SIZE * sizeof(int), C, 0, NULL, NULL);
 
 	// Clean up
 	ret = clFlush(command_queue);
